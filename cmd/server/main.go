bcdf0f1af898b6bf587c119a912e923652007b08
kullai-secasure <kullai.m@secasure.com>
2026-07-14 19:25:28 +0530

    Create main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"wikiflex/internal/handlers"
+	"wikiflex/internal/middleware"
+	"wikiflex/internal/storage"
+)
+
+func main() {
+	db := storage.NewPostgresDB()
+	defer db.Close()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/wiki/", handlers.ViewPage(db))
+	mux.HandleFunc("/edit/", handlers.EditPage(db))
+	mux.HandleFunc("/api/pages", handlers.ListPages(db))
+
+	wrapped := middleware.Logger(middleware.Auth(mux))
+	log.Println("WikiFlex started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", wrapped))
+}
